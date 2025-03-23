47ab0168cad5bc4d457c76112b10a717967aa163
Kevin <kevinjmathewskj@gmail.com>
2025-03-23 01:54:41 +0000

    pipeline

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -2,3 +2,4 @@ from utils.constants import REDDIT_SECRET, REDDIT_CLIENT_ID
 
 def reddit_pipeline(file_name:str, subreddit:str, time_filter='day', limit=None):
     instance = connect_reddit(REDDIT_SECRET, REDDIT_CLIENT_ID)
+    pass

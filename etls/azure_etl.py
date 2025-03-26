8a1b03a3e18314c2ef1849bf9c2b120653f66b2e
Kevin <kevinjmathewskj@gmail.com>
2025-03-26 02:29:25 +0000

    azure etl

--- etls/azure_etl.py
+++ etls/azure_etl.py
@@ -6,20 +6,16 @@ def connect_to_storage_account(connect_string:str):
     blob_service_client = BlobServiceClient.from_connection_string(connect_string)
     return blob_service_client
 
-def create_container(blob_service_client:BlobServiceClient.from_connection_string, container_name:str):
+def create_container(blob_service_client, container_name:str):
     try:
         blob_service_client.create_container(name=container_name)
         print('Container created')
     except ResourceExistsError:
         print('A container with this name already exists')
 
-def upload_blob(blob_service_client:BlobServiceClient.from_connection_string, container_name:str, file_path:str, file_name:str):
-    # upload_file_path = "./data/output/reddit_20250325.csv"
-    # file_name = 'new data.csv'
-
+def upload_blob(blob_service_client, container_name:str, file_path:str, file_name:str):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
-
-    print("\nUploading to Azure Storage as blob:\n\t" + file_name)
+    print(f'Uploading {file_name} to Azure Storage as blob')
 
     # Upload the created file
     with open(file=file_path, mode="rb") as data:
